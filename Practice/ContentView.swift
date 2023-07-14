ffbe2c869b8975c4e920a878bfcc94c775976716
rachel-156 <rltimes10@gmail.com>
2023-07-14 10:24:38 -0400

    Added my own stack with my own message of "Hi everyone!"

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hi everyone!")
+                .font(.title)
+                .foregroundColor(Color.green)
         }
-        .padding()
     }
 }
 
